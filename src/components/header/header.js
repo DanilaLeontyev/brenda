838a2a9450b2c0f0ce23567c74b24e040835a759
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-07-09 20:31:24 +0500

    добавил нормалайз

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,4 +1,8 @@
-import headerImg from './header.png';
+import BgImg from './image/bg-image.png';
+import Logotype from './image/logotype.png';
 
-let header = document.getElementById('header');
-header.src = headerImg;
+let headerBgImage = document.getElementById('header-bg-image');
+let headerLogotype = document.getElementById('header-logotype');
+
+headerBgImage.src = BgImg;
+headerLogotype.src = Logotype;
