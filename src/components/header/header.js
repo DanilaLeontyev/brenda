b46cfb3cc2916b270ce1f17c771c319460639889
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-07-09 21:24:13 +0500

    добавил несколько элементов

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,11 @@
 import BgImg from './image/bg-image.png';
 import Logotype from './image/logotype.png';
+import TelephoneLogo from './image/phone.svg';
 
 let headerBgImage = document.getElementById('header-bg-image');
 let headerLogotype = document.getElementById('header-logotype');
+const headerTelephoneButton = document.getElementById('telephone-logo');
 
 headerBgImage.src = BgImg;
 headerLogotype.src = Logotype;
+headerTelephoneButton.src = TelephoneLogo;
