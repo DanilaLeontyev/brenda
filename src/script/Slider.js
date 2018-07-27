d75c9c765d78c374f1cd2aa09c524538b463b7a9
Danila Leontyev <danila.leontyev.web@gmail.com>
2018-07-27 15:50:31 +0500

    сделал слайдер для блока atmospher

--- src/script/Slider.js
+++ src/script/Slider.js
@@ -1,14 +1,25 @@
 const nextButton = document.querySelector('#AtmosphereNext');
 const prevButton = document.querySelector('#AtmospherePrevious');
 const sliderContainer = document.querySelector('.gallery--imageWrapper');
+const imgArr = document.querySelectorAll('.gallery--imageWrapper>img');
+
+let imgCount = 0;
+let imgMargin = 30;
 
 function scrollRightSlider() {
   let r = sliderContainer.style.right.replace('px', '');
   if (r) {
     r = parseInt(r);
   }
-  r += 50;
+  if (imgCount === imgArr.length - 1) {
+    sliderContainer.style.right = '0px';
+    imgCount = 0;
+    return;
+  }
+
+  r += imgArr[imgCount].clientWidth + imgMargin;
   sliderContainer.style.right = r + 'px';
+  imgCount++;
 }
 
 function scrollLeftSlider() {
@@ -16,8 +27,26 @@ function scrollLeftSlider() {
   if (r) {
     r = parseInt(r);
   }
-  r -= 50;
+
+  if (imgCount === 0) {
+    imgArr.forEach(img => {
+      r += img.clientWidth + imgMargin;
+      r = +r;
+      console.log(r);
+    });
+    imgCount = imgArr.length - 1;
+    r -= imgArr[imgArr.length - 1].clientWidth;
+    sliderContainer.style.right = r + 'px';
+    return;
+  }
+
+  r -= imgArr[imgCount - 1].clientWidth + imgMargin;
   sliderContainer.style.right = r + 'px';
+  imgCount--;
 }
+
 nextButton.addEventListener('click', scrollRightSlider);
 prevButton.addEventListener('click', scrollLeftSlider);
+
+nextButton.addEventListener('touchstart', scrollRightSlider);
+prevButton.addEventListener('touchstart', scrollLeftSlider);
